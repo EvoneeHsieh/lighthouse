e8c9d2d988a398a3623098e069da860d7da0d542
EvoneeHsieh <154952104+EvoneeHsieh@users.noreply.github.com>
2024-12-13 14:05:48 +0800

    水位動畫成功
    拔了他的靜態就能啟動了

--- Assets/_Script/PlayerMovement.cs
+++ Assets/_Script/PlayerMovement.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody playerRb;
+    public void OnTriggerExit(Collider other)
+    {
+        ClimbProvider climbProvider = GetComponent<ClimbProvider>();
+        if (climbProvider != null && climbProvider.locomotionPhase != LocomotionPhase.Moving)
+        {
+            playerRb.useGravity = true;
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Water"))
